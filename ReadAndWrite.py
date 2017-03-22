4f0a267977624d3eb37f81083cfa1a2b9733bb55
Daniel Szwimer <danielszwimer@gmail.com>
2017-03-22 10:38:05 -0400

    I am just changing something and adding a comment.
    There was an issue with the comments
    I changed it so that it works
    Maybe we could not do this in the future.
    
    Closes-Bug: #0

--- ReadAndWrite.py
+++ ReadAndWrite.py
@@ -2,7 +2,7 @@ fw = open('sample.txt','w')
 fw.write('Just a test yo!\n')
 fw.write('Looks like its working.')
 fw.close()
-
+#this shouldnt change anything.
 fr = open('sample.txt','r')
 text = fr.read()
 print(text)
